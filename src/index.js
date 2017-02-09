699df6bab3695e0adf64513916b4f7e283a96d43
zZissou <kyle.edward.russell@gmail.com>
2017-02-09 15:13:58 -0800

    added components files for search bar, video details, video list items, and video list. Added youtube search function.

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = '';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
+
+// Create a new component. This component should produce some HTML
+const App = () => {
+  return (
+    <div>
+      <SearchBar></SearchBar>
+    </div>
+    );
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM).
+
+ReactDOM.render(<App></App>, document.querySelector('.container'));
